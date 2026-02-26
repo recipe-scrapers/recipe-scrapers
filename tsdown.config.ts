ec7fc69b77578a04e6dfd0684e00b1fdba496dcc
Justin Williams <justinwilliams42@gmail.com>
2026-02-26 07:41:03 -0800

    chore: update tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
+  outDir: 'dist',
+  format: 'esm',
   dts: true,
-  format: ['esm'],
   sourcemap: true,
   skipNodeModulesBundle: true,
 })
